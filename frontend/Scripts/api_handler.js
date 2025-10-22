9125a243d71f1b1c26625483d2456fdeb5383e9d
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-10-22 16:25:50 -0600

    Geocoding API implementation

--- frontend/Scripts/api_handler.js
+++ frontend/Scripts/api_handler.js
@@ -1,3 +1,46 @@
-fetch('https://api.openweathermap.org/data/3.0/onecall?lat=43.82&lon=-111.79&units=imperial&exclude=hourly,daily&appid=96d1b0cebb92c0268171d4eb02a4e776')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+const apiKey = "7452b20bcd78b372c5fd56d46266531e";
+
+const submitButton = document.querySelector("#searchButton");
+
+const separators = /[,\s]+/;
+
+let cityName = "";
+let stateName = "";
+let countryName = "";
+
+let queryString = "";
+
+function convertInputToObjects () {
+    const searchQuery = document.getElementById("searchBox");
+    const inputValue = searchQuery.value.toUpperCase();
+
+    const locationCode = inputValue.split(separators);
+
+    cityName = locationCode[0];
+    stateName = locationCode[1];
+    countryName = locationCode[2];
+
+    console.log(locationCode)
+
+    convertObjectsToString();
+}
+
+function convertObjectsToString() {
+    const objects = [cityName, stateName, countryName].filter(Boolean);
+    let queryString = objects.join(",")
+
+    convertLocationToCords(queryString);
+
+    console.log(queryString);
+}
+
+function convertLocationToCords(string) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${string}&limit=1&appid=${apiKey}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch(error => console.error('Error:', error));
+}
+
+submitButton.addEventListener("click", () => {
+    convertInputToObjects();
+});
