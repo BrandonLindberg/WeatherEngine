01a25b111b5308338c9077a0a9fbfe544a29a220
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-10-22 23:25:22 -0600

    api handler gets location and converts it to coordinates, and then gets the current conditions of that location

--- frontend/Scripts/api_handler.js
+++ frontend/Scripts/api_handler.js
@@ -1,17 +1,17 @@
-const apiKey = "7452b20bcd78b372c5fd56d46266531e";
+const apiKey = '';
 
-const submitButton = document.querySelector("#searchButton");
+const submitButton = document.querySelector('#searchButton');
 
 const separators = /[,\s]+/;
 
-let cityName = "";
-let stateName = "";
-let countryName = "";
+let cityName = '';
+let stateName = '';
+let countryName = '';
+let queryString = '';
 
-let queryString = "";
-
+// Take the location input and convert it into separate objects
 function convertInputToObjects () {
-    const searchQuery = document.getElementById("searchBox");
+    const searchQuery = document.getElementById('searchBox');
     const inputValue = searchQuery.value.toUpperCase();
 
     const locationCode = inputValue.split(separators);
@@ -20,27 +20,54 @@ function convertInputToObjects () {
     stateName = locationCode[1];
     countryName = locationCode[2];
 
-    console.log(locationCode)
-
     convertObjectsToString();
 }
 
+// Convert the objects into an array, filtering out any null or undefined values, and convert it to one string properly formatted for the geocode API call
 function convertObjectsToString() {
     const objects = [cityName, stateName, countryName].filter(Boolean);
-    let queryString = objects.join(",")
+    let queryString = objects.join(',');
 
     convertLocationToCords(queryString);
 
     console.log(queryString);
 }
 
-function convertLocationToCords(string) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${string}&limit=1&appid=${apiKey}`)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch(error => console.error('Error:', error));
+// Convert the location into coordinates
+async function convertLocationToCords(query) {
+    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}`;
+    const result = await fetch(url);
+
+    if (!result.ok) {
+        console.log(`HTTP ${result.status}: ${result.statusText}`);
+    }
+
+    const data = await result.json();
+
+    if (!Array.isArray(data) || data.length === 0) {
+        ('No location found');
+    }
+
+    const {lat, lon} = data[0];
+
+    getConditionsFromCoords(lat, lon);
+    console.log(`latitude: ${lat}, longitude: ${lon}`);
 }
 
-submitButton.addEventListener("click", () => {
+// Get current weather conditions for location from coordinates
+async function getConditionsFromCoords(lat, lon) {
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,daily,alerts&appid=${apiKey}`;
+    const result = await fetch(url);
+
+    if (!result.ok) {
+        console.log(`HTTP ${result.status}: ${result.statusText}`);
+    }
+
+    const data = await result.json();
+
+    console.log(data);
+}
+
+submitButton.addEventListener('click', () => {
     convertInputToObjects();
 });
